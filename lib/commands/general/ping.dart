ced10eca2b3da326d97ea0ead8e2d0e968c0270f
newtox <futzy261[1;3q@gmail.com>
2024-10-10 15:23:30 +0200

    Made selection optional

--- lib/commands/general/ping.dart
+++ lib/commands/general/ping.dart
@@ -24,23 +24,21 @@ final ping = ChatCommand(
       Locale.zhCn: "一个简单的 ping 命令来检查机器人的延迟。",
       Locale.ja: "ボットの遅延を確認するためのシンプルなピンのコマンド。",
       Locale.ko: "봇의 지연 시간을 확인하기 위한 간단한 핑 명령어."
-    }, (
-  ChatContext context,
-  @UseConverter(latencyTypeConverter)
-  @Description("The type of latency to view.", {
-    Locale.da: "Den type responstid, der skal vises.",
-    Locale.de: "Der Typ der Latenz, die angezeigt werden soll.",
-    Locale.enUs: "The type of latency to view.",
-    Locale.esEs: "El tipo de latencia que se desea ver.",
-    Locale.fr: "Le type de latence à afficher.",
-    Locale.ru: "Тип задержки для просмотра.",
-    Locale.hi: "देखने के लिए लेटेंसी का प्रकार।",
-    Locale.zhCn: "要查看的延迟类型。",
-    Locale.ja: "表示する遅延の種類。",
-    Locale.ko: "보기 위한 지연 유형."
-  })
-  String? selection,
-) async {
+    }, (ChatContext context,
+        [@UseConverter(latencyTypeConverter)
+        @Description("The type of latency to view.", {
+          Locale.da: "Den type responstid, der skal vises.",
+          Locale.de: "Der Typ der Latenz, die angezeigt werden soll.",
+          Locale.enUs: "The type of latency to view.",
+          Locale.esEs: "El tipo de latencia que se desea ver.",
+          Locale.fr: "Le type de latence à afficher.",
+          Locale.ru: "Тип задержки для просмотра.",
+          Locale.hi: "देखने के लिए लेटेंसी का प्रकार।",
+          Locale.zhCn: "要查看的延迟类型。",
+          Locale.ja: "表示する遅延の種類。",
+          Locale.ko: "보기 위한 지연 유형."
+        })
+        String? selection]) async {
   selection ??= await context.getSelection<String>(
     ["Basic", "Real", "Gateway"],
     MessageBuilder(content: await getString(context.user, "ping_latency")),
